f3f7b1979d8c9d219f90a63fda19e095bef6b680
Jan Cikryt <cikryt.jan@seznam.cz>
2022-11-09 12:24:17 +0100

    Added second table and updated primary_final_table

--- SQL_project_1.sql
+++ SQL_project_1.sql
@@ -2,7 +2,7 @@
  * First table
  */
 CREATE TABLE IF NOT EXISTS t_payroll AS (
-	SELECT cp.payroll_year AS Rok, 
+	SELECT cp.payroll_year AS Platy_rok, 
 		cpib.name AS Profesni_odvetvi, 
 		round(avg(cp.value)) AS Prumerny_plat
 	FROM czechia_payroll cp 
@@ -12,10 +12,29 @@ CREATE TABLE IF NOT EXISTS t_payroll AS (
 	GROUP BY cp.payroll_year, cpib.name
 	ORDER BY cpib.code, cp.payroll_year 
 );
--- Add first table to primary_final_table
-CREATE TABLE IF NOT EXISTS t_jan_cikryt_project_SQL_primary_final AS (
-	SELECT *
-	FROM t_payroll
+/*
+ * Second table
+ */
+CREATE TABLE IF NOT EXISTS t_food_prices AS (
+	SELECT 
+		cpc.name AS Kategorie_potravin, 
+		cp.value AS Cena, 
+		cpc.price_value AS Mnozstvi_potraviny, 
+		cpc.price_unit AS Jednotka, 
+		YEAR(cp.date_from) AS Potraviny_rok 
+	FROM czechia_price cp 
+	JOIN czechia_price_category cpc 
+		ON cp.category_code = cpc.code 
+	WHERE region_code IS NULL
+	GROUP BY date_from, cpc.name 
+	ORDER BY cp.category_code, date_from
 );
-
-
+/*
+ Add tables to primary_final_table
+*/
+CREATE OR REPLACE TABLE t_jan_cikryt_project_SQL_primary_final AS (
+	SELECT *
+	FROM t_payroll pr
+	JOIN t_food_prices tfp 
+		ON pr.Platy_rok = tfp.Potraviny_rok 
+);
